3d9037d548a38b0e8b626c3aed0d2b81be5fd490
Kamruzzaman Tutul <kamruzzaman103>
2025-01-07 17:44:06 +0600

    Final update t

--- src/components/VisaCard.jsx
+++ src/components/VisaCard.jsx
@@ -1,38 +1,4 @@
 
-
-// const VisaCard = ({ visa, onUpdate, onDelete }) => {
-//   return (
-//     <div className="border rounded-lg p-4">
-//       <img
-//         src={visa.countryImage}
-//         alt={visa.countryName}
-//         className="w-full h-32 object-cover rounded mb-4"
-//       />
-//       <h3 className="text-xl font-semibold mb-2">{visa.countryName}</h3>
-//       <p className="text-gray-700 mb-2">Visa Type: {visa.visaType}</p>
-//       <p className="text-gray-700 mb-2">Processing Time: {visa.processingTime}</p>
-//       <p className="text-gray-700 mb-2">Fee: ${visa.fee}</p>
-//       <p className="text-gray-700 mb-4">Validity: {visa.validity}</p>
-
-//       <div className="flex justify-between">
-//         <button
-//           onClick={onUpdate}
-//           className="bg-yellow-500 text-white py-1 px-4 rounded"
-//         >
-//           Update
-//         </button>
-//         <button
-//           onClick={onDelete}
-//           className="bg-red-500 text-white py-1 px-4 rounded"
-//         >
-//           Delete
-//         </button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default VisaCard;
 import { useState } from "react";
 
 const VisaCard = ({ visa, onUpdate, onDelete }) => {
--- src/pages/VisaDetails.jsx
+++ src/pages/VisaDetails.jsx
@@ -1,6 +1,3 @@
-
-
-
 import { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
